ccff45ac96a8bc0120ae591fc509bd5cb2630939
apatrikis <apatrikis@users.noreply.github.com>
2015-06-05 18:31:25 +0200

    Removed System.out.println()

--- src/test/java/AnyTest.java
+++ src/test/java/AnyTest.java
@@ -9,6 +9,7 @@ import com.prodyna.pac.timetracker.Security;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.Date;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -19,16 +20,16 @@ public class AnyTest {
 
     @Test
     public void testHashingAlgorithm() throws NoSuchAlgorithmException {
-        System.out.println(Security.passwordHashSHA256("password1"));
-
+        String own = Security.passwordHashSHA256("password1");
         // from "glassfish-embedded-all-4.1.jar" -> Google "guava" lib
-        System.out.println(Hashing.sha256().hashString("password1", Charsets.UTF_8).toString());
+        String lib = Hashing.sha256().hashString("password1", Charsets.UTF_8).toString();
+
+        Assert.assertTrue(own.equals(lib));
     }
 
     @Test
     public void testDateFormatParsing() {
         Instant i = Instant.parse("2007-12-03T10:15:30.00Z");
-        Date d = Date.from(i);
-        System.out.println(d);
+        Date.from(i);
     }
 }
