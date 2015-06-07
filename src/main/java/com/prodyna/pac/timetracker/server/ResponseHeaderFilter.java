50114e6ce7822b4ceacbca66b5d05e49254ab91b
apatrikis <apatrikis@users.noreply.github.com>
2015-06-07 19:51:09 +0200

    Added logging and documentation.

--- src/main/java/com/prodyna/pac/timetracker/server/ResponseHeaderFilter.java
+++ src/main/java/com/prodyna/pac/timetracker/server/ResponseHeaderFilter.java
@@ -8,6 +8,8 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A {@link Provider} for intercepting an {@code Response}. This is used for
@@ -18,20 +20,27 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class ResponseHeaderFilter implements ContainerResponseFilter {
 
+    // Glassfisch app server exceptions on startup when injected
+    private static Logger log = LoggerFactory.getLogger(ResponseHeaderFilter.class);
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) {
-        setCORSHeader(requestContext, response);
+        setCORSHeader(response);
     }
 
     /**
      * Set {@code CORS} header to enable client/server communication.
      *
+     * @param response The response to populate.
+     *
      * @see
      * <a href=http://de.wikipedia.org/wiki/Cross-Origin_Resource_Sharing">CORS</a>
      */
-    private void setCORSHeader(ContainerRequestContext requestContext, ContainerResponseContext response) {
+    private void setCORSHeader(ContainerResponseContext response) {
+        log.debug("Addding CORS header");
         // for access by the Angular JS frontend:
         // http://stackoverflow.com/questions/10143093/origin-is-not-allowed-by-access-control-allow-origin
+        // https://blogs.oracle.com/theaquarium/entry/supporting_cors_in_jax_rs
         response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
         response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.getHeaders().putSingle("Access-Control-Allow-Headers", "Authorization, Content-Type");
